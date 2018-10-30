7f78649bbabbe2ace8e19769464ca06303f54e7f
ajinkya.kulkarni77@gmail.com <ajinkya.kulkarni77@gmail.com>
2018-10-30 12:26:47 -0700

    Implemented PUT and DELETE call on MessageResource

--- restful-api-jersey/src/main/java/com/cs/ajinkya/restful_api_jersey/resources/MessageResource.java
+++ restful-api-jersey/src/main/java/com/cs/ajinkya/restful_api_jersey/resources/MessageResource.java
@@ -3,8 +3,10 @@ package com.cs.ajinkya.restful_api_jersey.resources;
 import java.util.List;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -14,28 +16,39 @@ import com.cs.ajinkya.restful_api_jersey.model.Message;
 import com.cs.ajinkya.restful_api_jersey.service.MessageService;
 
 @Path("/messages")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class MessageResource {
 
 	MessageService messageService = new MessageService();
 
 	@GET
-	@Produces(MediaType.APPLICATION_JSON)
 	public List<Message> getMessages() {
 		return messageService.getAllMessages();
 	}
-	
+
 	@GET
-	@Produces(MediaType.APPLICATION_JSON)
 	@Path("/{messageId}")
 	public Message getMessage(@PathParam("messageId") long messageId) {
 		return messageService.getMessage(messageId);
 	}
-	
+
 	@POST
-	@Produces(MediaType.APPLICATION_JSON)
-	@Consumes(MediaType.APPLICATION_JSON)
 	public Message addMessage(Message message) {
 		return messageService.addMessage(message);
+	}
+
+	@PUT
+	@Path("/{messageId}")
+	public Message updateMessage(@PathParam("messageId") long messageId, Message message) {
+		message.setId(messageId);
+		return messageService.updateMessage(message);
+	}
+
+	@DELETE
+	@Path("/{messageId}")
+	public Message deleteMessage(@PathParam("messageId") long messageId) {
+		return messageService.removeMessage(messageId);
 	}
 
 }
