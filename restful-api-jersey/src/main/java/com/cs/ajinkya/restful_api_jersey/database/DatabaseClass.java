0fda27c0672f45ba82f0742f855ccd228b20688b
ajinkya.kulkarni77@gmail.com <ajinkya.kulkarni77@gmail.com>
2018-10-30 12:48:00 -0700

    Implementd profileservice, and profileresouce

--- restful-api-jersey/src/main/java/com/cs/ajinkya/restful_api_jersey/database/DatabaseClass.java
+++ restful-api-jersey/src/main/java/com/cs/ajinkya/restful_api_jersey/database/DatabaseClass.java
@@ -9,26 +9,27 @@ import com.cs.ajinkya.restful_api_jersey.model.Profile;
 public class DatabaseClass {
 
 	private static Map<Long, Message> messages = new HashMap<Long, Message>();
-	private static Map<Long, Profile> profiles = new HashMap<Long, Profile>();
+	private static Map<String, Profile> profiles = new HashMap<String, Profile>();
 
 	static {
 		Message m1 = new Message(1l, "Hey Aj", "Ajinkya");
 		Message m2 = new Message(2l, "Hey Yogya", "Yogesh");
 		messages.put(1l, m1);
-		messages.put(m2.getId(),m2);
-		
-		Profile p1 = new Profile(1l,"Ajinkya Kulkari", "Ajinkya", "Kulkarni");
-		Profile p2 = new Profile(2l,"Yogesh M", "Yogesh", "Mhase");
-		profiles.put(p1.getId(), p1);
-		profiles.put(p2.getId(), p2);
-		
+		messages.put(m2.getId(), m2);
+
+		Profile p1 = new Profile(1l, "ajinkya", "Ajinkya", "Kulkarni");
+		Profile p2 = new Profile(2l, "yogesh", "Yogesh", "Mhase");
+		profiles.put(p1.getProfileName(), p1);
+		profiles.put(p2.getProfileName(), p2);
+
 	}
+
 	public static Map<Long, Message> getMessages() {
 
 		return messages;
 	}
 
-	public static Map<Long, Profile> getProfiles() {
+	public static Map<String, Profile> getProfiles() {
 
 		return profiles;
 
---
+++ restful-api-jersey/src/main/java/com/cs/ajinkya/restful_api_jersey/service/ProfileService.java
@@ -0,0 +1,40 @@
+package com.cs.ajinkya.restful_api_jersey.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import com.cs.ajinkya.restful_api_jersey.database.DatabaseClass;
+import com.cs.ajinkya.restful_api_jersey.model.Profile;
+
+public class ProfileService {
+
+	private Map<String, Profile> profiles = DatabaseClass.getProfiles();
+
+	public List<Profile> getAllProfiles() {
+		return new ArrayList<Profile>(profiles.values());
+	}
+
+	public Profile getProfile(String profileName) {
+		return profiles.get(profileName);
+	}
+
+	public Profile addMessage(Profile profile) {
+		profile.setId(profiles.size() + 1);
+		profiles.put(profile.getProfileName(), profile);
+		return profile;
+	}
+
+	public Profile updateProfile(Profile profile) {
+		if (profile.getProfileName().isEmpty()) {
+			return null;
+		}
+		profiles.put(profile.getProfileName(), profile);
+		return profile;
+	}
+
+	public Profile removeProfile(String profileName) {
+		return profiles.remove(profileName);
+	}
+
+}
