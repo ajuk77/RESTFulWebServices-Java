975ee7d471e315e188fd2232ed9dea736d9ab866
ajinkya.kulkarni77@gmail.com <ajinkya.kulkarni77@gmail.com>
2018-10-29 22:47:16 -0700

    implemented json response

--- restful-api-jersey/src/main/java/com/cs/ajinkya/restful_api_jersey/resources/MessageResource.java
+++ restful-api-jersey/src/main/java/com/cs/ajinkya/restful_api_jersey/resources/MessageResource.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -16,9 +17,16 @@ public class MessageResource {
 	MessageService messageService = new MessageService();
 
 	@GET
-	@Produces(MediaType.APPLICATION_XML)
+	@Produces(MediaType.APPLICATION_JSON)
 	public List<Message> getMessages() {
 		return messageService.getAllMessages();
+	}
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/{messageId}")
+	public Message getMessage(@PathParam("messageId") long messageId) {
+		return messageService.getMessage(messageId);
 	}
 
 }
