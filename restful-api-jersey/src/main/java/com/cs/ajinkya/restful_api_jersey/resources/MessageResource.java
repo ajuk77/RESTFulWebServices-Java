05c98d533e5e4a264bcc216f270cbc6e05dc646f
ajinkya.kulkarni77@gmail.com <ajinkya.kulkarni77@gmail.com>
2018-10-29 22:54:49 -0700

    Implemented POST call

--- restful-api-jersey/src/main/java/com/cs/ajinkya/restful_api_jersey/resources/MessageResource.java
+++ restful-api-jersey/src/main/java/com/cs/ajinkya/restful_api_jersey/resources/MessageResource.java
@@ -2,7 +2,9 @@ package com.cs.ajinkya.restful_api_jersey.resources;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -27,6 +29,13 @@ public class MessageResource {
 	@Path("/{messageId}")
 	public Message getMessage(@PathParam("messageId") long messageId) {
 		return messageService.getMessage(messageId);
+	}
+	
+	@POST
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Message addMessage(Message message) {
+		return messageService.addMessage(message);
 	}
 
 }
